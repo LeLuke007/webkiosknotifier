9642e79f642b6dafac21e82a3d2c76363c08d024
DarkHero <100042726+LeLuke007@users.noreply.github.com>
2023-12-05 23:01:40 +0530

    Update checkmarks.py

--- checkmarks.py
+++ checkmarks.py
@@ -20,26 +20,29 @@ with requests.Session() as c:
     "Password": os.getenv("WebkioskPassword"),
     }
 
-    c.post(loginurl,logindata,headers)
-    page=c.get(marksurl)
-    tr= int(page.text.count('tr')/2)
-    print(tr)
-    if tr>current:
-        try:
-            msg = """Subject: New Marks Uploaded """
-            fromaddr = os.getenv("emailID")
-            password = os.getenv("emailIDPassword")
-            with open("maillist.txt","r") as f:
-            	toaddrs=f.read().strip().split("\n")
+    
+    while(True):
+        with open("currentVal.txt","r") as f:
+            current=int(f.read())
+        c.post(loginurl,logindata,headers)
+        page=c.get(marksurl)
+        tr= int(page.text.count('<tr>')-4)
+        if tr>current:
+            try:
+                msg = """Subject: New Marks Uploaded """
+                fromaddr = os.getenv("emailID")
+                password = os.getenv("emailIDPassword")
+                with open("maillist.txt","r") as f:
+                    toaddrs=f.read().strip().split("\n")
         
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            server.login(fromaddr, password)
+                server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                server.login(fromaddr, password)
         
-            server.sendmail(fromaddr, toaddrs, msg)
-            with open("currentVal.txt","w") as f:
-                f.write(str(tr))
-            server.quit()
-        except:
-            print("Error Occured While Sending Mail")
-            server.quit()
-
+                server.sendmail(fromaddr, toaddrs, msg)
+                with open("currentVal.txt","w") as f:
+                    f.write(str(tr))
+                server.quit()
+                print("Marks Uploaded",tr)
+            except:
+                print("Error Occured While Sending Mail")
+                server.quit()
